dcf6bbee9ed53b321edbb323b703e1364cb5b542
Alfredo Cossetin <76018038+GalileuGalilei@users.noreply.github.com>
2022-06-06 22:50:46 -0300

    organizada

--- SrSapo.pl
+++ SrSapo.pl
@@ -1,11 +1,11 @@
 %https://olimpiada.ic.unicamp.br/static/extras/obi2021/provas/ProvaOBI2021_f3pj.pdf
 
-in_jump_range((X1,Y1), (X2,Y2), Table) :-
-    member((X2,Y2), Table), in_range((X1,Y1),(X2,Y2)).
-
 in_range((X1,Y1), (X2,Y2)) :-
     (X1 == X2, Y1 + 3 >= Y2);
     (Y1 == Y2, X1 + 3 >= X2).
+
+in_jump_range((X1,Y1), (X2,Y2), Table) :-
+    member((X2,Y2), Table), in_range((X1,Y1),(X2,Y2)).
 
 connection((X1,Y1), (XS, YS), [(X2,Y2)|[]], _) :-
     in_range((X1,Y1), (XS, YS)), (X2,Y2) = (XS, YS),!.
